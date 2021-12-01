e71abd4bb9678bf15ed01298e6d9522260fe862e
Vlada <81084648+zlures@users.noreply.github.com>
2021-12-01 20:31:42 +0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -3,3 +3,12 @@ Evest.JS @loader
 :: Copyright 2021 EvestJS
           made w/ love
 ———————————————**/
+
+const tester = document.createElement('div');
+tester.classList.add('evest-center');
+const body = document.body
+body.appendChild(div);
+const in = `
+<link rel="stylesheet" href="https://zlures.github.io/evest.js/css/loader.css"></link>
+<h1 class="textfocusin">Evest.JS</h1>
+`
