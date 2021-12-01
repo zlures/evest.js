a615d794b0f1b3cf09f7e99b54a1c20ca5b2e4da
Vlada <81084648+zlures@users.noreply.github.com>
2021-12-01 20:34:54 +0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -7,8 +7,9 @@ Evest.JS @loader
 const tester = document.createElement('div');
 tester.classList.add('evest-center');
 const body = document.body
-body.appendChild(div);
+body.appendChild(tester);
 const in = `
 <link rel="stylesheet" href="https://zlures.github.io/evest.js/css/loader.css"></link>
 <h1 class="textfocusin">Evest.JS</h1>
 `
+tester.innerHTML = in;
